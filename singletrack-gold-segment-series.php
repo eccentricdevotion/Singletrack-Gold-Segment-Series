64722e3f075bff0d3c15e8369a31aefd9c00da40
GraemeWoodward <graeme.woodward@canterbury.ac.nz>
2018-12-02 19:45:33 +1300

    Add Crocodile segment and remove Moving Time
    Add results for the Crocodile XC segment and remove the column for Moving Time, as only Elapsed Time counts.

--- singletrack-gold-segment-series.php
+++ singletrack-gold-segment-series.php
@@ -50,7 +50,7 @@ class Singletrack_Gold_Segments_Widget extends WP_Widget {
 		$singletrack_gold_strava_segment_ids = array( "Greenwood Climb" => 18563649,
 			"Rapaki Climb" => 18593281,
 			"Kennedys Climb" => 18897478,
-			"Crocodile XC" => -1,
+			"Crocodile XC" => 19310391,
 			"Living Springs XC" => 18563613,
 			"McLeans Island XC" => 18607655,
 			"Port Hills XC" => 18792223,
@@ -149,12 +149,14 @@ function singletrack_gold_is_curl_installed() {
 function singletrack_gold_getactivities( $json ) {
 
 	$entries = $json[ "entries" ];
-	echo "\n<tr><th class='singletrack_gold_th'>Who</th><th class='singletrack_gold_th'>When</th><th class='singletrack_gold_th'>Moving Time</th><th class='singletrack_gold_th'>Elapsed Time</th></tr>";
+	//echo "\n<tr><th class='singletrack_gold_th'>Who</th><th class='singletrack_gold_th'>When</th><th class='singletrack_gold_th'>Moving Time</th><th class='singletrack_gold_th'>Elapsed Time</th></tr>";
+	echo "\n<tr><th class='singletrack_gold_th'>Who</th><th class='singletrack_gold_th'>When</th><th class='singletrack_gold_th'>Elapsed Time</th></tr>";
+	
 	foreach ( $entries as $key => $values ) {
 		echo "\n<tr>";
 		echo "<td>" . $values[ "athlete_name" ] . "</td>";
 		echo "<td>" . format_date( $values[ 'start_date_local' ] ) . "</td>";
-		echo "<td>" . seconds_to_time( $values[ 'moving_time' ] ) . "</td>";
+		//echo "<td>" . seconds_to_time( $values[ 'moving_time' ] ) . "</td>";
 		echo "<td>" . seconds_to_time( $values[ 'elapsed_time' ] ) . "</td>";
 		echo "</tr>";
 	}
